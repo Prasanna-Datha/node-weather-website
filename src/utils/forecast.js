1acc63103ddd45d45ffbe515529a37fe072a03ef
prasanna.datha@apcogsys.net <prasanna.datha@gmail.com>
2019-05-17 09:51:36 +0530

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,11 @@ const forecast = (latitude, longitude, callback) => {
         body.daily.data[0].summary +
         " It is currently " +
         body.currently.temperature +
-        " \xB0C. There is " +
+        " \xB0C. The high today is " +
+        body.daily.data[0].temperatureHigh +
+        " with a low of " +
+        body.daily.data[0].temperatureLow +
+        ". There is " +
         body.currently.precipProbability +
         "% chance of rain.";
       callback(undefined, forecastInfo);
@@ -25,22 +29,3 @@ const forecast = (latitude, longitude, callback) => {
 };
 
 module.exports = forecast;
-/* const url =
-  "https://api.darksky.net/forecast/f9485b5f409802f38618cf4cafa4a16e/37.8267,-122.4233?units=si"; // &lang=kn
-
-request({ url: url, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to weather service!");
-  } else if (response.body.error) {
-    console.log("Unable to fine location!");
-  } else {
-    const currently = response.body.currently;
-    const daily = response.body.daily;
-    console.log(
-      "%s It is currently %f \xB0C. There is %f% chance of rain.",
-      daily.data[0].summary,
-      currently.temperature,
-      currently.precipProbability
-    );
-  }
-}); */
